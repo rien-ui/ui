29a344f4f8e6a8e41ed8551a4d83091552b97104
Kihyun Ryu <khryu0610@gmail.com>
2024-07-03 14:49:55 +0900

    Fix ungiven dependencies to createContextScope

--- packages/patterns/packages/react/context/src/createContext.tsx
+++ packages/patterns/packages/react/context/src/createContext.tsx
@@ -37,7 +37,7 @@ interface CreateScope {
   (): ScopeHook;
 }
 
-function createContextScope(scopeName: string, createContextScopeDeps: CreateScope[]) {
+function createContextScope(scopeName: string, createContextScopeDeps: CreateScope[] = []) {
   let defaultContexts: any[] = [];
 
   /**
